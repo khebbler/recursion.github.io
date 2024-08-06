2b9fc97940b3754a92011121ef72aa05486f156f
khebbler <khebbler@gmail.com>
2024-08-05 19:07:56 -0500

    saving recursion

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -5,16 +5,48 @@
 // Example:  5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5);  // 120
 var factorial = function(n) {
+  if (n < 0) {
+    return null;
+  }
+  // base 
+  if (n === 0) {
+    return 1;
+  }
+  // recursion
+  return n * factorial(n - 1);
 };
 
 // 2. Compute the sum of an array of integers.
 // Example:  sum([1, 2, 3, 4, 5, 6]);  // 21
 var sum = function(array) {
+  // base
+  if (array.length === 0) {
+    return 0;
+  }
+  // recursion
+  return array[0] + sum(array.slice(1));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // Example: arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  // base
+  if (n === 0) {
+    return true;
+  }
+  if (n === 1) {
+    return false;
+  }
+  if (n === -1) {
+    return false;
+  }
+
+  // recursion
+  if (n > 0) {
+    return isEven(n - 2)
+  } else 
+  return isEven(n + 2);
+
 };
 
 // 4. Check if a number is even.
